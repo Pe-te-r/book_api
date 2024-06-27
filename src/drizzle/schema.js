4ae76fd508f438eecefcf51cb0647d66223ec544
Pe-te-r <mburupeter8526@gmail.com>
2024-06-27 16:08:23 +0300

    better version

--- src/drizzle/schema.js
+++
@@ -1,7 +0,0 @@
-import { pgTable, integer, varchar } from 'drizzle-orm/pg-core';
-export const booksDetailsTable = pgTable("booksDetails", {
-    id: integer("id").primaryKey(),
-    title: varchar("title"),
-    author: varchar("author"),
-    year: varchar("year")
-});
